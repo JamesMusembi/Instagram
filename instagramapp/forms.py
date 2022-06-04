e2a5798000a92984358d19beb5b666934c32e805
JamesMusembi <james.musembi@student.moringaschool.com>
2022-06-04 17:57:20 +0300

    creating  form-profile

---
+++ instagramapp/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Image, Profile, Comments
+
+class NewImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ['comments', 'profile','name']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+        
+class NewProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = []        
