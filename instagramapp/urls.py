20188a2384eb8a07a15b6b04b4f41572b49b980e
JamesMusembi <james.musembi@student.moringaschool.com>
2022-06-04 14:09:59 +0300

    adding urls

--- instagramapp/urls.py
+++ instagramapp/urls.py
@@ -4,12 +4,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
-    url(r'^search/', views.search_results, name='search_results'),
-    url(r'^new/image$', views.new_post, name='new_post'),
-    url(r'^new/profile$', views.profile, name='profile'),
-    url(r'^myaccount$',views.myaccount,name = 'myaccount'),
-    url(r'^new/edit_profile$', views.edit_profile, name='edit_profile'),
+    # url('^$',views.welcome,name = 'welcome'),
+    # url(r'^search/', views.search_results, name='search_results'),
+    # url(r'^new/image$', views.new_post, name='new_post'),
+    # url(r'^new/profile$', views.profile, name='profile'),
+    # url(r'^myaccount$',views.myaccount,name = 'myaccount'),
+    # url(r'^new/edit_profile$', views.edit_profile, name='edit_profile'),
     
 
 
