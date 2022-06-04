3edd8667d5365e2ec77f9d10d8cc5cc7c95b40e6
JamesMusembi <james.musembi@student.moringaschool.com>
2022-06-04 16:56:45 +0300

    creating first model-profile

--- instagramapp/models.py
+++ instagramapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
 
 # Create your models here.
+class Profile(models.Model):
+    profile = models.ImageField(upload_to = 'images/',null=True)
+    bio = models.CharField(max_length =60)
+    username = models.ForeignKey(User,on_delete=models.CASCADE, null=True)
