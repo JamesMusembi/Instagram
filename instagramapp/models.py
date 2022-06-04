315da8a34653c502fe49be74f1a8ae57421ac5ea
JamesMusembi <james.musembi@student.moringaschool.com>
2022-06-04 17:05:42 +0300

    creating first model-profile

--- instagramapp/models.py
+++ instagramapp/models.py
@@ -8,3 +8,15 @@ class Profile(models.Model):
     profile = models.ImageField(upload_to = 'images/',null=True)
     bio = models.CharField(max_length =60)
     username = models.ForeignKey(User,on_delete=models.CASCADE, null=True)
+    
+    def __str__(self):
+        return self.bio
+
+    def save_profile(self):
+        self.save()    
+        
+    def update_profile(self):
+        self.update()
+
+    def delete_profile(self):
+        self.delete() 
