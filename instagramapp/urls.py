9c188037b80a9eb101a9d123d7c361b45c85daf9
JamesMusembi <james.musembi@student.moringaschool.com>
2022-06-04 14:06:19 +0300

    adding views

--- instagramapp/urls.py
+++ instagramapp/urls.py
@@ -1,6 +1,16 @@
-from django.urls import path
-from instagramapp import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url
+from . import views
 
-urlpatterns = [
-    path('', views.index, name="index")
+urlpatterns=[
+    url('^$',views.welcome,name = 'welcome'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^new/image$', views.new_post, name='new_post'),
+    url(r'^new/profile$', views.profile, name='profile'),
+    url(r'^myaccount$',views.myaccount,name = 'myaccount'),
+    url(r'^new/edit_profile$', views.edit_profile, name='edit_profile'),
+    
+
+
 ]
