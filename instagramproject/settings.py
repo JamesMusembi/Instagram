97f6fb014f2846a194758cb144437fac7443226b
JamesMusembi <james.musembi@student.moringaschool.com>
2022-06-04 23:59:13 +0300

    uploading images

--- instagramproject/settings.py
+++ instagramproject/settings.py
@@ -12,8 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config,Csv
 
-import django_bootstrap5
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
