11c4c3cd5fd79525dc2e4c66fe7e2865b9004581
JamesMusembi <james.musembi@student.moringaschool.com>
2022-06-04 12:25:43 +0300

    adding boostrap

--- instagramproject/settings.py
+++ instagramproject/settings.py
@@ -37,11 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_boostrap5'
     
     
     
     #own
-    'post',
+    'instagramapp',
 ]
 
 MIDDLEWARE = [
@@ -137,3 +138,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
